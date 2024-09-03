e40cffa3b8bb8019b49ebf2d837d777e6125e8e9
DaniloNogueira-Silva <danilo.nogueira1802@gmail.com>
2024-09-03 13:45:38 -0300

    Revert "feat: :sparkles: Creating wallet nestjs module"
    This reverts commit 8add19280e2a00a9881c268a1c3540afbd76ac76.

--- src/nest-modules/wallet-module/dto/create-walllet.dto.ts
+++
@@ -1,3 +0,0 @@
-import { CreateWalletInput } from "@core/wallet/application/use-cases/create-wallet/create-wallet.input";
-
-export class CreateWalletDto extends CreateWalletInput {}
--- src/nest-modules/wallet-module/dto/update-walllet.dto.ts
+++
@@ -1,8 +0,0 @@
-import { UpdateWalletInput } from "@core/wallet/application/use-cases/update-wallet/update-wallet.input";
-import { OmitType } from "@nestjs/mapped-types";
-
-export class UpdateWalletInputWithoutId extends OmitType(UpdateWalletInput, [
-  "id",
-] as const) {}
-
-export class UpdateWalletDto extends UpdateWalletInputWithoutId {}
