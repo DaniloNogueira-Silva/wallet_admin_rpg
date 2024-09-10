3d871daf1df378ab4c2e35447f161cc9fc4764b3
DaniloNogueira-Silva <danilo.nogueira1802@gmail.com>
2024-09-10 20:58:49 -0300

    fix: :bug: Fixing 24

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { applyGlobalConfig } from "./nest-modules/global-config";
+import 'mysql2';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
