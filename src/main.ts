b3c5b0e3e68db4ab139a403e1a184fe1dddc44ba
DaniloNogueira-Silva <danilo.nogueira1802@gmail.com>
2024-09-10 19:53:59 -0300

    fix: :bug: Fixing 17

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { applyGlobalConfig } from "./nest-modules/global-config";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+export async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: process.env.NODE_ENV === "development" ? console : undefined,
+  });
   await app.listen(3000);
 
   applyGlobalConfig(app);
